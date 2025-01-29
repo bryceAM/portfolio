009e62f84a9099ad47672b79581f1788e235add3
Bryce <bryce.malmberg@gmail.com>
2025-01-28 22:51:24 -0600

    feat 1: initial html content, layout, and css styles for the site

--- Portfolio/Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+252a420de00f86546d9e6883c10a1a15489b1bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5661c67c5d9b134fa9041bd077091a94cac688f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
